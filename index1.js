3d6053e2d9a68c2e9bcc803d76c66d8bc53d1421
vikawal <108365967+vikawal@users.noreply.github.com>
2022-08-05 16:30:04 +0200

    new boarder questions

--- index1.js
+++ index1.js
@@ -16,9 +16,17 @@ console.log(anotherHeight);
 rectangle.style.height = anotherHeight;
 
 
-let anotherBorder = prompt('Which border of rectangle do you want? Write CSS styles for border: dotted, dashed, solid, double, none, etc');
+let anotherBorder = prompt('Which border of rectangle do you want? Write CSS styles for border: dotted, dashed, solid, double, groove, inset, ridge etc');
 console.log(anotherBorder);
 rectangle.style.border = anotherBorder;
+
+let borderColour = prompt ('Which border of rectangle do you want? Write color name or color CSS code, example: red' );
+console.log(borderColour);
+rectangle.style.borderColor = borderColour;
+
+let borderWidth = prompt('Which width of the rectangle border in px do you want? Write number and px, example: 10px');
+console.log (borderWidth);
+rectangle.stylele.borderWidth = borderWidth;
 
 let firstValue = prompt ('Enter first value of the sum', '');
 let secondValue = prompt ('Enter second value of the sum', '');
