318006ce0f65dbbd0fec64501b078265f9ab59e5
vikawal <108365967+vikawal@users.noreply.github.com>
2022-08-06 19:52:58 +0200

    task 1 and 2

--- index.js
+++ index.js
@@ -1,28 +1,91 @@
-var userName = 'Vika Wal';
-var userAge = 36;
-var bool = true;
+//var userName = 'Vika Wal';
+//var userAge = 36;
+//var bool = true;
 
-var Array1 = ['Vika Wal', 36,true];
-var Array2 = [userName,userAge];
-var colors2 = ['black', 'white', 'tabby'];
+//var Array1 = ['Vika Wal', 36,true];
+//var Array2 = [userName,userAge];
+//var colors2 = ['black', 'white', 'tabby'];
 
-var Animal = {
-    typeOfAnimal:'Dog',
-    paws: 4,
-    canMeow: false,
-    canBark: true,
-    colors: colors2,
-    weight: '10kg',
-    baby: {
-        typeOfAnimal: 'Puppy',
-        paws: 4,
-        tail: 1,
-        weight: '2kg',
-    },
-    owner: {
-        name: userName,
-    }
-}
+//var Animal = {
+    //typeOfAnimal:'Dog',
+    //paws: 4,
+    //canMeow: false,
+    //canBark: true,
+    //colors: colors2,
+    //weight: '10kg',
+    //baby: {
+        //typeOfAnimal: 'Puppy',
+        //paws: 4,
+        //tail: 1,
+        //weight: '2kg',
+    //},
+   // owner: {
+       // name: userName,
+   // }
+//}
 
 //alert (Animal.typeOfAnimal + ' is parent of ' + Animal.baby.typeOfAnimal);
 //console.log(Animal);
+
+//task 2 variant 1
+//let a = prompt ('Enter first value (A)', '');
+//let b = prompt ('Enter second value (B)', '');
+//let c = prompt ('Enter third value (C)', '');
+//let message1 = (+a > +b && +a > +c) ? 'Biggest value is A':
+//(+b > +a && +b > +c) ? 'Biggest value is B':
+//(+c > +a && +c > +b) ? 'Biggest value is C':
+//(+a == +b && +b == +c)? 'Values are equal':
+//(+a == +b && +a > +c)? 'Biggest values are A and B, they are equal':
+//(+a == +b && +a < +c)? 'Biggest value is C, values A and B are equal':
+//(+a == +c && +a > +b)? 'Biggest values are A and C, they are equal':
+//(+a == +c && +a < +b)? 'Biggest value is B, values A and C are equal':
+//(+b == +c && +b > +a)? 'Biggest values are B and C, they are equal':
+//(+b == +c && +b < +a)? 'Biggest value is A, values B and C are equal':
+//'Mistake';
+//alert (message1);
+
+//task 2 variant 2
+//let a = prompt ('Enter first value (A)', '');
+//let b = prompt ('Enter second value (B)', '');
+//let c = prompt ('Enter third value (C)', '');
+//if (+a > +b && +a > +c) {
+ //message1 = 'Biggest value is A';
+ //}
+//else if (+b > +a && +b > +c) {
+//message1 = 'Biggest value is B';
+//}
+//else if (+c > +a && +c > +b) {
+    //message1 = 'Biggest value is C';
+//}
+//else if (+a == +b && +b == +c) {
+   // message1 = 'Values are equal';
+//}
+//else if (+a == +b && +a > +c) {
+    //message1 = 'Biggest values are A and B, they are equal';
+//}
+//else if (+a == +b && +a < +c) {
+ //   message1 = 'Biggest value is C, values A and B are equal';
+//}
+//else if (+a == +c && +a > +b) {
+    //message1 = 'Biggest values are A and C, they are equal';
+//}
+//else if (+a == +c && +a < +b) {
+ //   message1 = 'Biggest value is B, values A and C are equal';
+//}
+//else if  (+b == +c && +b > +a) {
+ //  message1 = 'Biggest values are B and C, they are equal'; 
+//}
+//else if (+b == +c && +b < +a) {
+//message1 = 'Biggest value is A, values B and C are equal';
+//}
+//else {
+ //  message1 = 'Mistake';
+//}
+//alert (message1);
+
+//Task 2 variant 3
+let a = prompt ('Enter first value (A)', '');
+let b = prompt ('Enter second value (B)', '');
+let c = prompt ('Enter third value (C)', '');
+let result = Math.max(+a, +b, +c);
+alert (result);
--- index1.js
+++ index1.js
@@ -1,3 +1,4 @@
+//Task 1 variant 1
 //let numberOfMonth = prompt ('Write number of the month to see the season', '');
 //let message = (numberOfMonth==12 || numberOfMonth==1 || numberOfMonth==2) ? 'It is WINTER':
 //(numberOfMonth > 2 && numberOfMonth <6) ? 'It is SPRING':
@@ -7,20 +8,21 @@
 
 //alert (message);
 
-let numberOfMonth = prompt ('Write number of the month to see the season', '');
-if (numberOfMonth==12 || numberOfMonth==1 || numberOfMonth==2) {
-    message = 'It is WINTER';
-}
-else if (numberOfMonth > 2 && numberOfMonth <6) {
-    message = 'It is SPRING';  
-}
-else if (numberOfMonth >= 6 && numberOfMonth <= 8) {
-    message = 'It is SUMMER';   
-}
-else if (numberOfMonth >=9 && numberOfMonth <= 11) {
-    message = 'It is AUTUMN';  
-}
-else {
-    message = 'Mistake';
-}
-alert (message);
+//Task 1 variant 2
+//let numberOfMonth = prompt ('Write number of the month to see the season', '');
+//if (numberOfMonth==12 || numberOfMonth==1 || numberOfMonth==2) {
+//    message = 'It is WINTER';
+//}
+//else if (numberOfMonth > 2 && numberOfMonth <6) {
+//    message = 'It is SPRING';  
+//}
+//else if (numberOfMonth >= 6 && numberOfMonth <= 8) {
+//    message = 'It is SUMMER';   
+//}
+//else if (numberOfMonth >=9 && numberOfMonth <= 11) {
+//    message = 'It is AUTUMN';  
+//}
+//else {
+//    message = 'Mistake';
+//}
+// alert (message);
